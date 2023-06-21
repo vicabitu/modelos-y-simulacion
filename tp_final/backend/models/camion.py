2b52ace859467c5f22545123aefd90b7fb44cfe3
Victor <victor.ch370@gmail.com>
2023-06-21 14:21:18 -0300

    Agregar atributo id en la clase Camion

--- tp_final/backend/models/camion.py
+++ tp_final/backend/models/camion.py
@@ -4,18 +4,19 @@ from constants import (
 )
 
 class Camion:
-    def __init__(self, tipo, materia_prima=True):
+    def __init__(self, tipo, id, materia_prima=True):
         self.tipo = tipo
         self.materia_prima = materia_prima #materia prima o producto terminado
         self.carga_neta = 0
+        self.id = id
     
     def set_carga(self):
         pesaje = self.calcular_pesaje_segun_tipo_de_camion()
-        if pesaje < PESO_CAMIONES.get(self.tipo)['sin_carga']:
-            pesaje = PESO_CAMIONES.get(self.tipo)['sin_carga']
-        elif pesaje > PESO_CAMIONES.get(self.tipo)['peso_maximo']:
-            pesaje = PESO_CAMIONES.get(self.tipo)['peso_maximo']
-        self.carga_neta = pesaje - PESO_CAMIONES.get(self.tipo)['sin_carga']
+        if pesaje < PESO_CAMIONES.get(self.tipo).get('sin_carga'):
+            pesaje = PESO_CAMIONES.get(self.tipo).get('sin_carga')
+        elif pesaje > PESO_CAMIONES.get(self.tipo).get('peso_maximo'):
+            pesaje = PESO_CAMIONES.get(self.tipo).get('peso_maximo')
+        self.carga_neta = pesaje - PESO_CAMIONES.get(self.tipo).get('sin_carga')
 
     def calcular_pesaje_segun_tipo_de_camion(self):
         media = PESAJES_CAMIONES.get(self.tipo).get('media')
@@ -28,4 +29,4 @@ class Camion:
         self.carga_neta = peso_maximo - peso_minimo
     
     def __str__(self):
-        return f'Tipo: {self.tipo} - Carga: {self.carga_neta}'
+        return f'ID: {self.id} - Tipo: {self.tipo} - Carga: {self.carga_neta}'
